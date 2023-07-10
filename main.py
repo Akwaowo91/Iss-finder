fa66b7d422358fffae0509179809196772c729d9
Akwaowo91 <akwaowoudokop15@gmail.com>
2023-07-10 16:52:40 -0500

    this hold all the code, and it's a very time specific code, it should be ran based on the timing in the code

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def is_night():
         "formatted": 0,
     }
 
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
